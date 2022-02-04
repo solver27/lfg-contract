a8ac4a99707df6333ba0d9a5280b556c0a86f160
solver27 <m.leonel0507@gmail.com>
2022-02-04 12:08:50 +0800

    Initial design of data struct

--- contracts/SAMContract.sol
+++ contracts/SAMContract.sol
@@ -13,7 +13,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SAMContract is Ownable, ReentrancyGuard {
 
-    event ListingPlaced(bytes32 indexed listingId, address indexed hostContract, address indexed sender, uint tokenId,
+    event ListingPlaced(address indexed sender, address indexed hostContract, uint tokenId,
         uint auctionStartPrice, uint buyNowPrice, string uri);
 
     struct bidding {
@@ -22,7 +22,7 @@ contract SAMContract is Ownable, ReentrancyGuard {
         uint timestamp;         // The timestamp user create the bidding
     }
 
-    struct listing {
+    struct listingInfo {
         address seller;         // The owner of the NFT who want to sell it
         address hostContract;   // The source of the contract
         uint tokenId;           // The NFT token ID
@@ -30,13 +30,31 @@ contract SAMContract is Ownable, ReentrancyGuard {
         uint buyNowPrice;       // The price user can directly buy the NFT
         uint timestamp;         // The timestamp of the listing creation
         uint auctionDuration;   // The duration of the biddings, in seconds
-        bidding [] biddings;    // The list of biddings
+    }
+
+    struct listing {
+        listingInfo info;
+        address seller;         // The owner of the NFT who want to sell it
+        address hostContract;   // The source of the contract
+        uint tokenId;           // The NFT token ID
+        uint auctionStartPrice; // The auction start price
+        uint buyNowPrice;       // The price user can directly buy the NFT
+        uint timestamp;         // The timestamp of the listing creation
+        uint auctionDuration;   // The duration of the biddings, in seconds
+        mapping(uint => bidding) biddings; // The bidding of the listing, to workaround the array issue
+        uint countOfBidding;    // The count of bidding
+    }
+
+    struct listings {
+        mapping(uint => listing) lists;
+        uint countOfLists;
     }
 
     address operator;
 
-    uint listingNonce;
-    mapping (bytes32 => listing) nftListing;
+    address [] addresses;
+
+    mapping (address => listings) addrNftListing;
 
     constructor (address _owner) {
         _transferOwnership(_owner);
@@ -47,18 +65,52 @@ contract SAMContract is Ownable, ReentrancyGuard {
         operator = _operator;
     }
 
-    function addListing(address _seller, address _hostContract, uint _tokenId, uint _startPrice, uint _buyNowPrice, uint _duration) external {
-        bytes32 listingId = keccak256(abi.encodePacked(listingNonce, _hostContract, _tokenId));
-        nftListing[listingId].seller = _seller;
-        nftListing[listingId].hostContract = _hostContract;
-        nftListing[listingId].tokenId = _tokenId;
-        nftListing[listingId].auctionStartPrice = _startPrice;
-        nftListing[listingId].buyNowPrice = _buyNowPrice;
-        nftListing[listingId].timestamp = block.timestamp;
-        nftListing[listingId].auctionDuration = _duration;
-        listingNonce += 1;
-        ERC721 hostContract = ERC721(nftListing[listingId].hostContract);
+    function addListing(address _hostContract, uint _tokenId, uint _startPrice, uint _buyNowPrice, uint _duration) external {
+        //bytes32 listingId = keccak256(abi.encodePacked(listingNonce, _hostContract, _tokenId));
+
+        // listing memory lst = listing({seller: msg.sender, hostContract : _hostContract, tokenId : _tokenId, auctionStartPrice : _startPrice,
+        //     buyNowPrice : _buyNowPrice, timestamp : block.timestamp, auctionDuration : _duration, countOfBidding: 0 });
+        //bidding[] storage biddings;
+        //bidding[] memory biddings = new bidding[](1);
+        //listing memory lst;// = listing(msg.sender, _hostContract, _tokenId, _startPrice, _buyNowPrice, block.timestamp, _duration, biddings);
+        //nftListing[listingId] = lst;
+        //listingNonce += 1;
+
+        addrNftListing[msg.sender].countOfLists = 0;
+        uint id = addrNftListing[msg.sender].countOfLists;
+        addrNftListing[msg.sender].lists[id].seller = msg.sender;
+        addrNftListing[msg.sender].lists[id].hostContract = _hostContract;
+        addrNftListing[msg.sender].lists[id].tokenId = _tokenId;
+        addrNftListing[msg.sender].lists[id].auctionStartPrice = _startPrice;
+        addrNftListing[msg.sender].lists[id].buyNowPrice = _buyNowPrice;
+        addrNftListing[msg.sender].lists[id].timestamp = block.timestamp;
+        addrNftListing[msg.sender].lists[id].auctionDuration = _duration;
+        addrNftListing[msg.sender].countOfLists++;
+
+        addresses.push(msg.sender);
+
+        // nftListing[listingId].seller = msg.sender;
+        // nftListing[listingId].hostContract = _hostContract;
+        // nftListing[listingId].tokenId = _tokenId;
+        // nftListing[listingId].auctionStartPrice = _startPrice;
+        // nftListing[listingId].buyNowPrice = _buyNowPrice;
+        // nftListing[listingId].timestamp = block.timestamp;
+        // nftListing[listingId].auctionDuration = _duration;
+
+        ERC721 hostContract = ERC721(_hostContract);
         string memory uri = hostContract.tokenURI(_tokenId);
-        emit ListingPlaced(listingId, _hostContract, _seller, _tokenId, _startPrice, _buyNowPrice, uri);
+        emit ListingPlaced(msg.sender, _hostContract, _tokenId, _startPrice, _buyNowPrice, uri);
+    }
+
+    function listingOfAddr(address addr) public view returns(listingInfo[] memory)
+    {
+        listingInfo[] memory infoLists = new listingInfo[](addrNftListing[addr].countOfLists);
+        for (uint i = 0; i < addrNftListing[addr].countOfLists; ++i) {
+            //listings.push(addrNftListing[addr].lists[i]);
+            //listing storage lst = addrNftListing[addr].lists[i];
+            infoLists[i] = addrNftListing[addr].lists[i].info;
+        }
+        return infoLists;
     }
 }
+
