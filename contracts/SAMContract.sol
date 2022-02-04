bb40eab10a5bdbd52804a276b2fa64be17ea7205
solver27 <m.leonel0507@gmail.com>
2022-02-04 19:55:24 +0800

    Use mapping to save listings and biddings

--- contracts/SAMContract.sol
+++ contracts/SAMContract.sol
@@ -6,6 +6,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -13,8 +14,21 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SAMContract is Ownable, ReentrancyGuard {
 
-    event ListingPlaced(address indexed sender, address indexed hostContract, uint tokenId,
+    event ListingPlaced(bytes32 indexed listingId, address indexed sender, address indexed hostContract, uint tokenId,
         uint auctionStartPrice, uint buyNowPrice, string uri);
+
+    event BiddingPlaced(bytes32 indexed biddingId, bytes32 listingId, uint price);
+
+    struct listing {
+        address seller;         // The owner of the NFT who want to sell it
+        address hostContract;   // The source of the contract
+        uint tokenId;           // The NFT token ID
+        uint auctionStartPrice; // The auction start price
+        uint buyNowPrice;       // The price user can directly buy the NFT
+        uint timestamp;         // The timestamp of the listing creation
+        uint auctionDuration;   // The duration of the biddings, in seconds
+        bytes32[] biddingIds;   // The array of the bidding Ids
+    }
 
     struct bidding {
         address bidder;         // User who submit the bidding
@@ -22,95 +36,95 @@ contract SAMContract is Ownable, ReentrancyGuard {
         uint timestamp;         // The timestamp user create the bidding
     }
 
-    struct listingInfo {
-        address seller;         // The owner of the NFT who want to sell it
-        address hostContract;   // The source of the contract
-        uint tokenId;           // The NFT token ID
-        uint auctionStartPrice; // The auction start price
-        uint buyNowPrice;       // The price user can directly buy the NFT
-        uint timestamp;         // The timestamp of the listing creation
-        uint auctionDuration;   // The duration of the biddings, in seconds
-    }
+    address [] userAddresses;
 
-    struct listing {
-        listingInfo info;
-        address seller;         // The owner of the NFT who want to sell it
-        address hostContract;   // The source of the contract
-        uint tokenId;           // The NFT token ID
-        uint auctionStartPrice; // The auction start price
-        uint buyNowPrice;       // The price user can directly buy the NFT
-        uint timestamp;         // The timestamp of the listing creation
-        uint auctionDuration;   // The duration of the biddings, in seconds
-        mapping(uint => bidding) biddings; // The bidding of the listing, to workaround the array issue
-        uint countOfBidding;    // The count of bidding
-    }
+    mapping (bytes32 => listing) listingRegistry;   // The mapping of listing Id to listing details
 
-    struct listings {
-        mapping(uint => listing) lists;
-        uint countOfLists;
-    }
+    mapping (address => bytes32[]) addrListingIds;  // The mapping of the listings of address
 
-    address operator;
+    mapping (bytes32 => bidding) biddingRegistry;   // The mapping of bidding Id to bidding details
 
-    address [] addresses;
+    mapping (address => bytes32[]) addrBiddingIds;  // The mapping of the bidding of address
 
-    mapping (address => listings) addrNftListing;
+    uint operationNonce;
 
-    constructor (address _owner) {
+    IERC20 public _lfgToken;
+
+    constructor (address _owner, IERC20 _token) {
         _transferOwnership(_owner);
-    }
-
-    function setOperator(address _operator) onlyOwner external
-    {
-        operator = _operator;
+        _lfgToken = _token;
     }
 
     function addListing(address _hostContract, uint _tokenId, uint _startPrice, uint _buyNowPrice, uint _duration) external {
-        //bytes32 listingId = keccak256(abi.encodePacked(listingNonce, _hostContract, _tokenId));
+        // TODO: transfer the NFT to Escrow contract
 
-        // listing memory lst = listing({seller: msg.sender, hostContract : _hostContract, tokenId : _tokenId, auctionStartPrice : _startPrice,
-        //     buyNowPrice : _buyNowPrice, timestamp : block.timestamp, auctionDuration : _duration, countOfBidding: 0 });
-        //bidding[] storage biddings;
-        //bidding[] memory biddings = new bidding[](1);
-        //listing memory lst;// = listing(msg.sender, _hostContract, _tokenId, _startPrice, _buyNowPrice, block.timestamp, _duration, biddings);
-        //nftListing[listingId] = lst;
-        //listingNonce += 1;
+        bytes32 listingId = keccak256(abi.encodePacked(operationNonce, _hostContract, _tokenId));
 
-        addrNftListing[msg.sender].countOfLists = 0;
-        uint id = addrNftListing[msg.sender].countOfLists;
-        addrNftListing[msg.sender].lists[id].seller = msg.sender;
-        addrNftListing[msg.sender].lists[id].hostContract = _hostContract;
-        addrNftListing[msg.sender].lists[id].tokenId = _tokenId;
-        addrNftListing[msg.sender].lists[id].auctionStartPrice = _startPrice;
-        addrNftListing[msg.sender].lists[id].buyNowPrice = _buyNowPrice;
-        addrNftListing[msg.sender].lists[id].timestamp = block.timestamp;
-        addrNftListing[msg.sender].lists[id].auctionDuration = _duration;
-        addrNftListing[msg.sender].countOfLists++;
+        listingRegistry[listingId].seller = msg.sender;
+        listingRegistry[listingId].hostContract = _hostContract;
+        listingRegistry[listingId].tokenId = _tokenId;
+        listingRegistry[listingId].auctionStartPrice = _startPrice;
+        listingRegistry[listingId].buyNowPrice = _buyNowPrice;
+        listingRegistry[listingId].timestamp = block.timestamp;
+        listingRegistry[listingId].auctionDuration = _duration;
+        operationNonce++;
 
-        addresses.push(msg.sender);
+        addrListingIds[msg.sender].push(listingId);
 
-        // nftListing[listingId].seller = msg.sender;
-        // nftListing[listingId].hostContract = _hostContract;
-        // nftListing[listingId].tokenId = _tokenId;
-        // nftListing[listingId].auctionStartPrice = _startPrice;
-        // nftListing[listingId].buyNowPrice = _buyNowPrice;
-        // nftListing[listingId].timestamp = block.timestamp;
-        // nftListing[listingId].auctionDuration = _duration;
+        userAddresses.push(msg.sender);
 
         ERC721 hostContract = ERC721(_hostContract);
         string memory uri = hostContract.tokenURI(_tokenId);
-        emit ListingPlaced(msg.sender, _hostContract, _tokenId, _startPrice, _buyNowPrice, uri);
+        emit ListingPlaced(listingId, msg.sender, _hostContract, _tokenId, _startPrice, _buyNowPrice, uri);
     }
 
-    function listingOfAddr(address addr) public view returns(listingInfo[] memory)
+    function listingOfAddr(address addr) public view returns(listing[] memory)
     {
-        listingInfo[] memory infoLists = new listingInfo[](addrNftListing[addr].countOfLists);
-        for (uint i = 0; i < addrNftListing[addr].countOfLists; ++i) {
-            //listings.push(addrNftListing[addr].lists[i]);
-            //listing storage lst = addrNftListing[addr].lists[i];
-            infoLists[i] = addrNftListing[addr].lists[i].info;
+        listing[] memory resultlistings = new listing[](addrListingIds[addr].length);
+        for (uint i = 0; i < addrListingIds[addr].length; ++i) {
+            bytes32 listingId = addrListingIds[addr][i];
+            resultlistings[i] = listingRegistry[listingId];
         }
-        return infoLists;
+        return resultlistings;
+    }
+
+    function placeBid(bytes32 listingId, uint price) external {
+        listing storage lst = listingRegistry[listingId];
+        require(lst.timestamp + lst.auctionDuration > block.timestamp, "The listing is expired");
+
+        require(price > lst.auctionStartPrice, "Bid price should larger than the auction start price");
+
+        // TODO: transfer the price amount of LFG token to Escrow contract
+
+        bytes32 biddingId = keccak256(abi.encodePacked(operationNonce, lst.hostContract, lst.tokenId));
+        biddingRegistry[biddingId].bidder = msg.sender;
+        biddingRegistry[biddingId].price = price;
+        biddingRegistry[biddingId].timestamp = block.timestamp;
+
+        lst.biddingIds.push(biddingId);
+
+        addrBiddingIds[msg.sender].push(biddingId);
+
+        emit BiddingPlaced(biddingId, listingId, price);
+    }
+
+    function biddingOfListing(bytes32 listingId) public view returns (bidding[] memory) {
+        listing storage lst = listingRegistry[listingId];
+        bidding[] memory bids = new bidding[](lst.biddingIds.length);
+        for (uint i = 0; i < lst.biddingIds.length; ++i) {
+            bytes32 biddingId = lst.biddingIds[i];
+            bids[i] = biddingRegistry[biddingId];
+        }
+        return bids;
+    }
+
+    function biddingOfAddr(address addr) public view returns(bidding[] memory)
+    {
+        bidding[] memory biddings = new bidding[](addrBiddingIds[addr].length);
+        for (uint i = 0; i < addrBiddingIds[addr].length; ++i) {
+            bytes32 biddingId = addrBiddingIds[addr][i];
+            biddings[i] = biddingRegistry[biddingId];
+        }
+        return biddings;
     }
 }
-
