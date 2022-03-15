f6b5a526c33927a7f3f609f9fb749c3e2d714775
solver27 <m.leonel0507@gmail.com>
2022-03-15 22:00:12 +0800

    Add LFGNFT1155 contract
    Add LFGNFT1155 contract

---
+++ contracts/LFGNFT1155.sol
@@ -0,0 +1,114 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract LFGNFT1155 is ERC1155, Ownable {
+    uint256 private _currentTokenID = 0;
+    mapping(uint256 => address) public creators;
+    mapping(uint256 => uint256) public tokenSupply;
+
+    constructor(address _owner, string memory uri_) ERC1155(uri_) {
+        require(_owner != address(0), "Invalid owner address");
+        _transferOwnership(_owner);
+    }
+
+    /**
+     * @dev Require msg.sender to be the creator of the token id
+     */
+    modifier creatorOnly(uint256 _id) {
+        require(creators[_id] == msg.sender, "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
+        _;
+    }
+
+    /**
+     * @dev Creates a new token type and assigns _initialSupply to an address
+     * NOTE: remove onlyOwner if you want third parties to create new tokens on your contract (which may change your IDs)
+     * @param _to address of the first owner of the token
+     * @param _initialSupply amount to supply the first owner
+     * @param _data Data to pass if receiver is contract
+     * @return The newly created token ID
+     */
+    function create(
+        address _to,
+        uint256 _initialSupply,
+        bytes calldata _data
+    ) external returns (uint256) {
+        uint256 _id = _getNextTokenID();
+        _incrementTokenTypeId();
+        creators[_id] = msg.sender;
+
+        _mint(_to, _id, _initialSupply, _data);
+        tokenSupply[_id] = _initialSupply;
+        return _id;
+    }
+
+    /**
+     * @dev Mints some amount of tokens to an address
+     * @param _to          Address of the future owner of the token
+     * @param _id          Token ID to mint
+     * @param _quantity    Amount of tokens to mint
+     * @param _data        Data to pass if receiver is contract
+     */
+    function mint(
+        address _to,
+        uint256 _id,
+        uint256 _quantity,
+        bytes memory _data
+    ) public creatorOnly(_id) {
+        _mint(_to, _id, _quantity, _data);
+        tokenSupply[_id] += _quantity;
+    }
+
+    /**
+     * @dev Mint tokens for each id in _ids
+     * @param _to          The address to mint tokens to
+     * @param _ids         Array of ids to mint
+     * @param _quantities  Array of amounts of tokens to mint per id
+     * @param _data        Data to pass if receiver is contract
+     */
+    function mintBatch(
+        address _to,
+        uint256[] memory _ids,
+        uint256[] memory _quantities,
+        bytes memory _data
+    ) public {
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 _id = _ids[i];
+            require(creators[_id] == msg.sender, "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
+            uint256 quantity = _quantities[i];
+            tokenSupply[_id] += quantity;
+        }
+        _mintBatch(_to, _ids, _quantities, _data);
+    }
+
+    function setURI(string memory newuri) external onlyOwner {
+        _setURI(newuri);
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenID
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenID() private view returns (uint256) {
+        return _currentTokenID + 1;
+    }
+
+    /**
+     * @dev increments the value of _currentTokenID
+     */
+    function _incrementTokenTypeId() private {
+        _currentTokenID++;
+    }
+
+    /**
+     * @dev Returns the total quantity for a token ID
+     * @param _id uint256 ID of the token to query
+     * @return amount of token in existence
+     */
+    function totalSupply(uint256 _id) public view returns (uint256) {
+        return tokenSupply[_id];
+    }
+}
