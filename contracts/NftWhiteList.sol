daf20a7035dd47ac55320ea4d6305aa587619813
solver27 <m.leonel0507@gmail.com>
2022-03-16 10:38:45 +0800

    NFTWhiteList contract support NFT 1155 contract

--- contracts/NftWhiteList.sol
+++ contracts/NftWhiteList.sol
@@ -8,15 +8,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./interfaces/INftWhiteList.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract NftWhiteList is Ownable, INftWhiteList {
     event SetNftContractWhitelist(address indexed nftContract, bool isWhitelist);
 
     // The NFT contract whitelists, only NFT contract whitelisted can sell in the marketplace
     mapping(address => bool) public nftContractWhiteLists;
-
-    // https://eips.ethereum.org/EIPS/eip-721
-    bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
 
     constructor(address _owner) {
         require(_owner != address(0), "Invalid owner address");
@@ -26,7 +25,8 @@ contract NftWhiteList is Ownable, INftWhiteList {
     function setNftContractWhitelist(address _addr, bool _isWhitelist) external onlyOwner {
         require(_addr != address(0), "Invalid NFT contract address");
         require(
-            IERC165(_addr).supportsInterface(_INTERFACE_ID_ERC721),
+            IERC165(_addr).supportsInterface(type(IERC721).interfaceId) ||
+                IERC165(_addr).supportsInterface(type(IERC1155).interfaceId),
             "Invalid NFT token contract address"
         );
 
