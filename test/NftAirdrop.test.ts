64c1357d01c9f5f20b9c373e921b36cc774fdfb4
solver27 <m.leonel0507@gmail.com>
2022-04-12 16:00:04 +0800

    Let user select which NFT can claim
    Let user select which NFT can claim

--- test/NftAirdrop.test.ts
+++ test/NftAirdrop.test.ts
@@ -1,10 +1,10 @@
-const {expect} = require("chai");
+const { assert, expect } = require("chai");
 const hre = require("hardhat");
-const {web3} = require("hardhat");
+const { web3 } = require("hardhat");
 const LFGFireNFTArt = hre.artifacts.require("LFGFireNFT");
 const NftAirdropArt = hre.artifacts.require("NftAirdrop");
 const BN = require("bn.js");
-const {createImportSpecifier} = require("typescript");
+const { createImportSpecifier } = require("typescript");
 
 describe("NftAirdrop", function () {
   let LFGFireNFT = null;
@@ -12,7 +12,7 @@ describe("NftAirdrop", function () {
   let accounts = ["", ""],
     minter;
 
-  const airDropNftAmount = 2;
+  const airDropNftAmount = 1;
 
   before("Deploy contract", async function () {
     try {
@@ -27,7 +27,9 @@ describe("NftAirdrop", function () {
 
   it("test claim NFT method", async function () {
     const today = Math.round(new Date() / 1000);
-    await hre.network.provider.send("evm_setNextBlockTimestamp", [today + 1000]);
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+      today + 1000,
+    ]);
     await hre.network.provider.send("evm_mine");
     await NftAirdrop.addWhitelists([accounts[1]], [airDropNftAmount]);
 
@@ -35,16 +37,39 @@ describe("NftAirdrop", function () {
     console.log("Get minter result ", minterResult.toString());
 
     const whitelistInfo = await NftAirdrop.whitelistPools(accounts[1]);
-    expect(new BN(whitelistInfo.nftAmount).toString()).to.equal(airDropNftAmount.toString());
+    expect(new BN(whitelistInfo.nftAmount).toString()).to.equal(
+      airDropNftAmount.toString()
+    );
     expect(new BN(whitelistInfo.distributedAmount).toString()).to.equal("0");
 
-    await NftAirdrop.claimDistribution({from: accounts[1]});
+    const claimTokenId = "2";
+
+    await expect(
+      NftAirdrop.claimDistribution(claimTokenId, { from: accounts[1] })
+    ).to.be.revertedWith("ERC721: operator query for nonexistent token");
+
+    await LFGFireNFT.adminMint(10, NftAirdrop.address, { from: accounts[0] });
+    let tokenIds = await LFGFireNFT.tokensOfOwner(NftAirdrop.address);
+    console.log("Tokens of airdrop contract: ", JSON.stringify(tokenIds));
+
+    await NftAirdrop.claimDistribution(claimTokenId, { from: accounts[1] });
+
+    tokenIds = await LFGFireNFT.tokensOfOwner(NftAirdrop.address);
+    console.log("Tokens of airdrop contract: ", JSON.stringify(tokenIds));
+
     const whitelistInfoAfter = await NftAirdrop.whitelistPools(accounts[1]);
-    expect(new BN(whitelistInfoAfter.nftAmount).toString()).to.equal(airDropNftAmount.toString());
-    expect(new BN(whitelistInfoAfter.distributedAmount).toString()).to.equal(airDropNftAmount.toString());
+    expect(new BN(whitelistInfoAfter.nftAmount).toString()).to.equal(
+      airDropNftAmount.toString()
+    );
+    expect(new BN(whitelistInfoAfter.distributedAmount).toString()).to.equal(
+      airDropNftAmount.toString()
+    );
 
     const nftBalance = await LFGFireNFT.balanceOf(accounts[1]);
     console.log("nftBalance ", nftBalance.toString());
     expect(new BN(nftBalance).toString()).to.equal(airDropNftAmount.toString());
+
+    let claimedToken = await LFGFireNFT.tokensOfOwner(accounts[1]);
+    assert.equal(claimedToken[0], claimTokenId);
   });
 });
