5331206eae37a6bc6177f7f80ca4def970409248
solver27 <m.leonel0507@gmail.com>
2022-04-14 22:55:10 +0800

    Deploy airdrop contract on bsc mainnet
    Deploy airdrop contract on bsc mainnet

--- args/NFTAirdropArg.js
+++ args/NFTAirdropArg.js
@@ -1,3 +1,3 @@
 module.exports = [
-    "0x2BAbB6586bBa97F2ca5Eea087CfB89d1E7Bd2c98"
+    "0x8d06c569beac76f2cf7a54e61157d1c461b9bf85"
   ];
--- hardhat.config.js
+++ hardhat.config.js
@@ -51,14 +51,14 @@ module.exports = {
       gasPrice: 20e9,
       gas: 35e5,
     },
-    // bsc: {
-    //   accounts: [process.env.MAINNET_PRIVKEY || ""],
-    //   chainId: 56,
-    //   url: "https://bsc-dataseed1.ninicoin.io/",
-    //   timeout: 999999,
-    //   gasPrice: 20e9,
-    //   gas: 35e5,
-    // },
+    bsc: {
+      accounts: [process.env.MAINNET_PRIVKEY || ""],
+      chainId: 56,
+      url: "https://bsc-dataseed1.ninicoin.io/",
+      timeout: 999999,
+      gasPrice: 20e9,
+      gas: 35e5,
+    },
   },
   etherscan: {
     apiKey: process.env.BSCSCAN_API
