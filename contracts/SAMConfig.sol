82d98f9cd01d3114bf16c2f303f9405d9f94c752
solver27 <m.leonel0507@gmail.com>
2022-04-21 13:28:50 +0900

    add event for all values updated

--- contracts/SAMConfig.sol
+++ contracts/SAMConfig.sol
@@ -30,32 +30,47 @@ abstract contract SAMConfig is Ownable, ISAMConfig {
     // The maximum duration
     uint256 public maxDuration;
 
+    event UpdatedRoyaltiesFeeRate(uint256 rate);
+    event UpdatedFireNftContractAddress(address addr);
+    event UpdatedRevenueAddress(address addr);
+    event UpdatedBurnAddress(address addr);
+    event UpdatedFeeBurnRate(uint256 rate);
+    event UpdatedMinDuration(uint256 duration);
+    event UpdatedMaxDuration(uint256 duration);
+
     function setRoyaltiesFeeRate(uint256 rate) external onlyOwner {
-        royaltiesFeeRate = rate;        
+        royaltiesFeeRate = rate;   
+        emit UpdatedRoyaltiesFeeRate(rate);    
     }
 
     function setFireNftContractAddress(address _address) external  onlyOwner {
         fireNftContractAddress = _address;
+        emit UpdatedFireNftContractAddress(_address);
     }
 
     function setRevenueAddress(address _address) external onlyOwner {
         revenueAddress = _address;
+        emit UpdatedRevenueAddress(_address);
     }
 
     function setBurnAddress(address _address) external onlyOwner {
         burnAddress = _address;
+        emit UpdatedBurnAddress(_address);
     }
 
     function setFeeBurnRate(uint256 _rate) external onlyOwner {
         feeBurnRate = _rate;
+        emit UpdatedFeeBurnRate(_rate);
     }
 
     function setMinDuration(uint256 _duration) external onlyOwner {
-        minDuration = _duration;        
+        minDuration = _duration; 
+        emit UpdatedMinDuration(_duration);       
     }
 
     function setMaxDuration(uint256 _duration) external onlyOwner {
         maxDuration = _duration;
+        emit UpdatedMaxDuration(_duration);     
     }
 
     function getBurnAddress() external view returns (address) {
